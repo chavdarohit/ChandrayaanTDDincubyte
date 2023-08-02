7a91e5ac3242cb3f612acaec90947a847ccd1fcd
Administrator <Administrator@SYSTEMA7.DCSRCC.com>
2023-08-02 16:44:33 +0530

    Test passed:- The spacecraft is in north and turning right it and return E

--- src/main/Chandrayaan.java
+++ src/main/Chandrayaan.java
@@ -27,18 +27,41 @@ public class Chandrayaan {
                 case 'f':
                     moveForward();
                     break;
-                    
+                case 'r':
+                    turnRight();
+                    break;
                 default:
                     System.out.println("Invalid command: " + command);
                     break;
             }
         }
         return finalans = Integer.toString(xDirect) +
-        Integer.toString(yDirect) +
-        Integer.toString(zDirect) +
-        Character.toString(direction);
+                Integer.toString(yDirect) +
+                Integer.toString(zDirect) +
+                Character.toString(direction);
 
-
+    }
+        private void turnRight() {
+        switch (direction) {
+            case 'N':
+                direction = 'E';
+                break;
+            case 'E':
+                direction = 'S';
+                break;
+            case 'U':
+                direction = 'E';
+                break;
+            case 'S':
+                direction = 'W';
+                break;
+            case 'W':
+                direction = 'N';
+                break;
+            case 'D':
+                direction = 'E';
+                break;
+        }
     }
 
     public void moveForward() {
